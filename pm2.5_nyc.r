54b1ca4172d54cbdd95a3b9c76acbe3b1c81edba
Maya-rae <marnott276@gmail.com>
2025-09-24 16:39:16 -0400

    cleaning up the datasets & combining only the data I need (pm2.5, RH & temp)

--- pm2.5_nyc.r
+++ pm2.5_nyc.r
@@ -8,6 +8,26 @@ library(dplyr)
 library(rstan)
 library(tidyverse)
 
-## importing dataset
-LES_PM25_df = 
-  read_csv("/data/openaq_LES_PM2.5.csv")
+## importing PM 2.5 dataset
+les_pm25_df = 
+  read_csv("data/openaq_LES_PM2.5.csv")
+les_pm25_df = 
+  janitor::clean_names(les_pm25_df)
+
+## importing RH dataset
+les_rh_df = 
+  read_csv("data/openaq_LES_RH.csv")
+les_rh_df = 
+  janitor::clean_names(les_rh_df)
+
+## importing Temp dataset
+les_temp_df = 
+  read_csv("data/openaq_LES_Temp.csv")
+les_temp_df = 
+  janitor::clean_names(les_temp_df)
+
+## Mutate dataset PM2.5
+
+selected_pm25 <- les_pm25_df |> 
+  select(location_id, parameter, value, datetime_local)
+
